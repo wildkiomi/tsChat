c7ff515fe1814d81e53b720b5e22837d0f246c8f
wildkiomi <whisann.lie14@gmail.com>
2020-10-01 10:35:42 +0300

    configuration changes, deployed on heroku

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
-const mongoose = require("mongoose");
 const expressHbs = require("express-handlebars");
 var bodyParser = require('body-parser');
 const hbs = require("hbs");
+const mongoose = require("mongoose");
 var expressWs = require('express-ws')(app);
 var swaggerUi = require('swagger-ui-express'),
 	swaggerDocument = require('../swagger.json');
 
 app.engine("hbs", expressHbs(
     {
-        layoutsDir: "views/layouts", 
+        layoutsDir: __dirname + "/views/layouts", 
         defaultLayout: "layout",
         extname: "hbs"
     }
@@ -19,6 +19,7 @@ app.engine("hbs", expressHbs(
 app.use(express.static(__dirname + "/static"));
 
 app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
 
 hbs.registerPartials(__dirname + "/views/partials");
 
@@ -38,100 +39,13 @@ app.use(function (req, res, next) {
     res.status(404).send("Not Found")
 });
 
-mongoose.connect("mongodb://localhost:27017/usersdb", { useNewUrlParser: true }, function(err){
-    if(err) return console.log(err);
-    app.listen(8080, function(){
-        console.log("Server is ready");
+mongoose.connect("mongodb://localhost:27017/usersdb", 
+	{ useNewUrlParser: true }, 
+	function(err) {
+    	if (err) {
+    		console.log(err)
+    	};
+    app.listen(process.env.PORT || 8080, function() {
+    	 console.log("Server is ready");
     });
 });
-
-
- 		/*if ((message.writer=="anon")&&(!register)){
- 			message.writer=uuidv4();
-			var user=new User({
-				name: message.writer,
-				id: message.writer
-			});
-			user.save(function(err){
-				if (err) throw err;
-					console.log(arguments);
-			});
-
-
-
- 		 	clientSockets.set(message.writer,ws);
- 		 	register=true;
- 		}
- 		else if ((message.writer!="anon")&&(!register)){
- 			auth(message.writer);
- 			clientSockets.set(message.writer,ws);
-			register=true;
- 		}
- 		else if ((message.writer=="operator")&&(register)){
- 			}
- 		else{
- 			addToDb(message.time,message.writer,message.value,message.toWho);*/
-
- 			
-
-
-/*function addToDb(time,writer,value,toWho){
-	var message=new History({
-		time: time,
-		writer: writer,
-		value: value,
-		toWho: toWho
-	});
-	message.save(function(err){
-				if (err) throw err;
-					console.log(arguments);
-			});
-};*/
-
-/*function auth(username){
-	var user=new User({
-				name: username,
-				id: uuidv4()
-			});
-	user.save(function(err){
-		if (err) throw err;
-			console.log(arguments);
-		});
-	if (username=="operator") sendDb();
-
-
-};
-	*/
-
-
- 
- /*clientSocket.on('close', function() {
- closeSocket();
- mongoose.disconnect();
- });
-
- process.on('SIGINT', function() {
-	 console.log("Closing things");
-	 closeSocket('Server has disconnected');
-	 process.exit();
- });*/
-
-/*const uuidv4 = require("uuid/v4");
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/tschat', {useNewUrlParser: true});
-var User=require("./user");
-var History=require("./history");
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  
-});*/
-
-/*var actionsWithDb=new Map();
-	actionsWithDb.set("sendDb", sendDb);
-	actionsWithDb.set("sortByName", sortByName);*/
-	//actionsWithDb.set("sendDb", sendDb);
-/*
-var url='https://wildkiomichat.firebaseio.com';
-var database = new Firebase(url);*/
